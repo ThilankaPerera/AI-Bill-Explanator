95c7eff8797d7f7a774bff2f47bfaddb036d04db
ThilankaPerera <142975696+ThilankaPerera@users.noreply.github.com>
2023-08-23 13:32:50 +0530

    Create test_child.py
    Creating a test python code for child branch

---
+++ test_child.py
@@ -0,0 +1,9 @@
+# Initialize a variable to store the sum
+sum_of_numbers = 0
+
+# Loop through numbers from 1 to 10
+for i in range(1, 11):
+    sum_of_numbers += i
+
+# Print the sum
+print("Sum of numbers from 1 to 10:", sum_of_numbers)
