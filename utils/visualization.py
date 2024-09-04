b557fedc281c9cdbc947bf243cebe56208afe2c9
ThilankaPerera <thilankainduranga@gmail.com>
2024-09-04 19:30:00 +0530

    Add utils modules: visualization

---
+++ utils/visualization.py
@@ -0,0 +1,30 @@
+import plotly.graph_objects as go
+import plotly.express as px
+from typing import Dict, List
+import pandas as pd
+
+
+class Visualizer:
+    """Create visualizations for bill data"""
+    
+    @staticmethod
+    def create_pie_chart(charges_summary: Dict) -> go.Figure:
+        """Create a pie chart of charge categories"""
+        if not charges_summary:
+            return None
+        
+        labels = list(charges_summary.keys())
+        values = list(charges_summary.values())
+        
+        fig = go.Figure(data=[go.Pie(
+            labels=labels,
+            values=values,
+            hole=0.3,
+            marker=dict(colors=px.colors.qualitative.Set3)
+        )])
+        
+        
+        
+        return fig
+    
+    
