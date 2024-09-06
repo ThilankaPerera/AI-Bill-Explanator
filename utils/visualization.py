ed12c328b564d91f1de1fdc40565b47248e4ee8f
ThilankaPerera <thilankainduranga@gmail.com>
2024-09-06 19:30:00 +0530

    Create a bar chart of charges

--- utils/visualization.py
+++ utils/visualization.py
@@ -32,4 +32,37 @@ class Visualizer:
         
         return fig
     
+    @staticmethod
+    def create_bar_chart(charges_summary: Dict) -> go.Figure:
+        """Create a bar chart of charges"""
+        if not charges_summary:
+            return None
+        
+        df = pd.DataFrame(list(charges_summary.items()), 
+                         columns=['Category', 'Amount'])
+        df = df.sort_values('Amount', ascending=False)
+        
+        fig = go.Figure(data=[go.Bar(
+            x=df['Category'],
+            y=df['Amount'],
+            marker=dict(
+                color=df['Amount'],
+                colorscale='Viridis',
+                showscale=True
+            ),
+            text=df['Amount'].apply(lambda x: f'Rs. {x:,.2f}'),
+            textposition='auto'
+        )])
+        
+        fig.update_layout(
+            title="Charges Comparison",
+            xaxis_title="Category",
+            yaxis_title="Amount (LKR)",
+            font=dict(size=12),
+            height=400,
+            showlegend=False
+        )
+        
+        return fig
+    
     
